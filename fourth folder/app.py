a77c7f2c8c28901e32e29497f1d5ca7bf696ddfe
Kunal Goyal <kunalgoyal727gmail.com@Kunals-MacBook-Air.local>
2026-09-18 22:06:10 +0530

    Integrated Asset Consolidator into main app

--- fourth folder/app.py
+++ fourth folder/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import streamlit as st
+
 
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Sales Dashboard", "Asset Consolidator"])
